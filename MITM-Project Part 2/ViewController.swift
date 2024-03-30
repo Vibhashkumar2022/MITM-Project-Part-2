883a6452c5dd78030a96963cc53048f700d75156
Vibhash Kumar <vibhash.kumar@tothenew.com>
2024-03-30 11:41:46 +0530

    Initial Commit

---
+++ MITM-Project Part 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MITM-Project Part 2
+//
+//  Created by Vibhash Kumar on 30/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
